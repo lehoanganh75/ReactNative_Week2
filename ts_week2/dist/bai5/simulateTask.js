5f727c79d63c02abae1ba06facf9d6dd5b9b545c
lehoanganh75 <gaokun2021@gmail.com>
2025-08-27 07:35:15 +0700

    5. Finish exercise SimulateTask

---
+++ ts_week2/dist/bai5/simulateTask.js
@@ -0,0 +1,7 @@
+"use strict";
+function simulateTask(time) {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve("Task done"), time);
+    });
+}
+simulateTask(2000).then(console.log);
