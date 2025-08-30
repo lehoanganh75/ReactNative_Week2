fd9481645e436af126c88957757fa833dcbf070c
lehoanganh75 <gaokun2021@gmail.com>
2025-08-30 10:00:49 +0700

    24. Finish exercise Post Data

---
+++ ts_week2/src/bai24/postData.ts
@@ -0,0 +1,9 @@
+async function postData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {"Content-Type":"application/json"},
+    body: JSON.stringify({title:"foo", body:"bar", userId:1})
+  });
+  console.log(await res.json());
+}
+postData();
