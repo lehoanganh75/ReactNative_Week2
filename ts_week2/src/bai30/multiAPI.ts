cfe6749199e4b2950c148393a9733da09ed84180
lehoanganh75 <gaokun2021@gmail.com>
2025-08-30 10:11:43 +0700

    30. Finish exercise Multiple API

---
+++ ts_week2/src/bai30/multiAPI.ts
@@ -0,0 +1,10 @@
+async function multiAPI() {
+  const urls = [
+    "https://jsonplaceholder.typicode.com/todos/1",
+    "https://jsonplaceholder.typicode.com/todos/2",
+    "https://wrong.url"
+  ];
+  const results = await Promise.allSettled(urls.map(u => fetch(u)));
+  results.forEach((res, i) => console.log(`API ${i+1}:`, res.status));
+}
+multiAPI();
